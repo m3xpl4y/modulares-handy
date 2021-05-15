a7f1031dd779193f3c7536bcb560a9efc6570006
m3xpl4y <65121055+m3xpl4y@users.noreply.github.com>
2021-05-15 22:43:05 +0200

    Klasse PhoneFiles hinzugefügt, div. Methoden hinzugefügt

--- Phone.cs
+++ Phone.cs
@@ -2,6 +2,33 @@ namespace Modulares_Handy
 {
     public class Phone
     {
-        
+        public string Color { get; set; }
+        //Constructor
+        public Phone(string color)
+        {
+            this.Color = color;
+        }
+
+        #region FUNCTIONS
+        public void TakePicture()
+        {
+
+        }
+
+        public string MakeCall()
+        {
+            return null;
+        }
+
+        public int GetFreeSpace()
+        {
+            return 0;
+        }
+
+        public void PrintAllFiles()
+        {
+
+        }
+        #endregion
     }
 }
--- SdCard.cs
+++ SdCard.cs
@@ -2,6 +2,25 @@ namespace Modulares_Handy
 {
     public class SdCard
     {
-        
+
+        public int Capacity { get; set; }
+        public SdCard()
+        {
+        }
+
+        #region FUNCTIONS
+        public void SaveFile(PhoneFile phoneFile)
+        {
+
+        }
+        public void GetAllFiles()//TODO: get List of Files(Create List)
+        {
+
+        }
+        public int GetFreeSpace()
+        {
+            return 0;
+        }
+        #endregion
     }
 }
